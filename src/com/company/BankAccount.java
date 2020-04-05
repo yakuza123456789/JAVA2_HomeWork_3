ada16a09d099322631021b0882979ea692f05b27
yakuza123456789 <aziko050050@gmail.com>
2020-04-05 22:19:55 +0600

    Версия 3

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -10,17 +10,15 @@ public class BankAccount {
 
     public double deposit(double sum){
         amount = amount + sum;
-        System.out.println("Вы положили в банк " + amount);
-        System.out.println("Ваш счет в банке "+ amount);
         return amount;
     }
 
     public int withDraw(int sum) throws LimitException{
-        System.out.println("Вы сняли " + sum);
-        amount = amount - sum;
+
         if (sum > amount){
-            throw new LimitException("Не хватает денежных стредств ", getAmount());
+            throw new LimitException("Не хватает денежных стредств "+sum, amount);
         }
+        amount = amount - sum;
         return sum;
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,18 +8,21 @@ public class Main {
         bankAzamat.deposit(10000);
 
         while (true){
-            try {
-                bankAzamat.withDraw(5000);
-                System.out.println("Ваш баланс состовляет: " + bankAzamat.getAmount());
-            } catch (LimitException le){
-                System.out.println("На вашем балансе: " + bankAzamat.getAmount());
-                double amount = bankAzamat.getAmount();
-                if (bankAzamat.getAmount() >= bankAzamat.getAmount())
-                    System.out.println("Вы сняли остаток суммы "+amount);
-                    amount = le.getRemainigAmount() - le.getRemainigAmount();
-                System.out.println("Ваш баланс составляет:" + amount);
-                break;
-            }
+           try {
+               System.out.println("Ваш баланс состовляет: " +bankAzamat.getAmount());
+               bankAzamat.withDraw(5000);
+           }catch (LimitException le){
+               System.out.println(le.getMessage());
+               try {
+                   bankAzamat.withDraw((int) bankAzamat.getAmount());
+               } catch (LimitException l){
+                   l.printStackTrace();
+               }
+               break;
+           }
+
         }
+
+        System.out.println("Остаток "+bankAzamat.getAmount());
     }
 }
