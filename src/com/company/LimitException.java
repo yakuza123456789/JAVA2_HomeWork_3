00ed40ef1533653eb53957d71dd176e07d8ace6a
yakuza123456789 <aziko050050@gmail.com>
2020-04-05 20:54:15 +0600

    Версия 2

--- src/com/company/LimitException.java
+++ src/com/company/LimitException.java
@@ -5,7 +5,8 @@ public class LimitException extends Exception{
     private double remainigAmount;
 
     public LimitException(String message, double remainigAmount){
-
+    super(message);
+    this.remainigAmount = remainigAmount;
     }
 
     public double getRemainigAmount(){
